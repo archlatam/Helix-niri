607769ba6749d319a9a6161071a65fbd32e38a46
archlatam <corelinuxx@gmail.com>
2026-06-04 14:09:46 -0300

    update profiledef and archiso.conf

--- helix/profiledef.sh
+++ helix/profiledef.sh
@@ -3,15 +3,17 @@ iso_name="helix"
 iso_label="HELIX_$(date +%Y%m)"
 iso_publisher="Helix Linux <https://helixlinux.dev>"
 iso_application="Helix Linux — niri + Noctalia Edition"
-iso_version="4.0"
+iso_version="5.0"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+## GRUB
+bootmodes=('bios.syslinux' 'uefi.grub')
+# bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-b' '1M')
+# airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
