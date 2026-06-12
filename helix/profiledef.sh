2c2abccee178e0be5609a91dbba1e7ce1f7bc325
archlatam <corelinuxx@gmail.com>
2026-06-12 16:14:40 -0300

    add install.sh user live

--- helix/profiledef.sh
+++ helix/profiledef.sh
@@ -19,5 +19,6 @@ file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/skel/install.sh"]="0:0:755"
   ["/root/customize_airootfs.sh"]="0:0:755"
 )
